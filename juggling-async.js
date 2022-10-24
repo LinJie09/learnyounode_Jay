24df40408f94f4a98e2bb592badb0707d7553b41
LinJie09 <zxc664231@gmail.com>
2022-10-24 21:14:19 +0800

    update-juggling-async

--- juggling-async.js
+++ juggling-async.js
@@ -1,8 +1,11 @@
 const http = require('http')
-const bl = require('bl')
+const bl = require('bl');
+const concat = require('concat-stream')
+const { resolve } = require('path/posix');
+const { LOADIPHLPAPI } = require('dns');
 const result = []
-let count = 0
-
+/*let count = 0*/
+/*
 function printResult() {
     for (let i = 0; i < 3; i++) {
         console.log(result[i]);
@@ -16,17 +19,37 @@ function httpGet(index) {
             }
             result[index] = data.toString()
             count++
-            if(count === 3){
+            if (count === 3) {
                 printResult()
             }
         }))
     })
 }
-for(let i = 0;i<3;i++){
+for (let i = 0; i < 3; i++) {
     httpGet(i)
+}*/
+
+async function get(url) {
+    return new Promise(resolve => {
+        http.get(url, res => resolve(res))
+    })
 }
 
-
+async function doit() {
+    for (let i = 2; i < process.argv.length; i++) {
+        const res = await get(process.argv[i])
+        res.setEncording('utf-8')
+        res.pipe(
+            concat(data =>{
+                result.push(data)
+            })
+        )
+    }
+    result.forEach(item =>{
+        console.log(item);
+    })
+}
+doit()
 
 
 
